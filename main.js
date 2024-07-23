ffc87baa4975efde32fb6f83d09237dd9d7b8689
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-23 11:25:32 +0330

    use swagger

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const SwaggerConfig = require("./src/config/swagger.config");
 
 dotenv.config();
 
 async function main() {
   const app = express();
 
-  // Connect to DB
+  //   Connect to DB
   require("./src/config/mongoose.config");
 
-  // Run Server
+  //   Use Swagger
+  SwaggerConfig(app);
+
+  //   Run Server
   const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log(`server: http://localhost:${PORT}`);
   });
 }
 main();
+
+// 1. Create and configure server
+// 2. Configure and conncet to db
+// 3. Configure and use Swagger
