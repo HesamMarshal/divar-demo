6ad85d0caf942bbfb95352b05696e2c5414cd201
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-24 11:32:29 +0330

    add logic for sendOTP

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,8 +1,38 @@
+const createHttpError = require("http-errors");
+const UserModel = require("../user/user.model");
+const AuthMessage = require("./auth.messages");
+const { randomInt } = require("crypto");
+
 class AuthService {
+  #model;
+
   constructor() {
     autoBind(this);
+    this.#model = UserModel;
   }
-  async sendOTP(mobile) {}
+  async sendOTP(mobile) {
+    const user = await this.#model.findOne({ mobile });
+
+    //  Create OTP
+    const now = new Date().getTime();
+    const otp = {
+      code: randomInt(10000, 99999),
+      expiresIn: now + 1000 * 60 * 2,
+    };
+
+    // Check if user not exist create new user,
+    // if user exsit check for otp and expires
+    if (!user) {
+      const newUser = await this.#model.create({ mobile, otp });
+      return newUser;
+    }
+    if (user.otp && user.otp.expiresIn > now)
+      throw new createHttpError.BadRequest(AuthMessage.OtpCodeNotExpired);
+
+    user.otp = otp;
+    await user.save();
+    return user;
+  }
 
   async checkOTP(mobile, code) {}
 }
