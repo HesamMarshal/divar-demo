0a4af8ec11c2850f7922c9d4a0e342990c47b680
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-24 11:29:30 +0330

    create some messages

---
+++ src/modules/auth/auth.messages.js
@@ -0,0 +1,7 @@
+const AuthMessage = {
+  SendOtpSuccessfully: "Send OTP succefully",
+  NotFound: "User Not Found",
+  OtpCodeNotExpired: "OTP code not expired please try later",
+};
+
+module.exports = AuthMessage;
