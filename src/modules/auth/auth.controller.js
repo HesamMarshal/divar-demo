6f461596af7859240cc78f8aa500d3978272781a
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-24 10:27:48 +0330

    template for authController

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,22 @@
+class AuthController {
+  constructor() {}
+  async sendOTP(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+  async checkOTP(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
