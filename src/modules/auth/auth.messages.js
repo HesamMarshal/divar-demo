13dfce4722d9c63321e7dd5539b8c97bdeb3ad10
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-25 13:08:31 +0330

    add messages for checkOTP

--- src/modules/auth/auth.messages.js
+++ src/modules/auth/auth.messages.js
@@ -2,6 +2,9 @@ const AuthMessage = {
   SendOtpSuccessfully: "OTP sent succefully",
   NotFound: "User Not Found",
   OtpCodeNotExpired: "OTP code not expired yet. please wait for a few seconds.",
+  OtpCodeExpired: "OTP code expired. please ask for new OTP.",
+  OtpCodeIsIncorrect: "OTP Code is incorrect.",
+  LoggedInSuccessfully: "You Logged in successfully.",
 };
 
 module.exports = AuthMessage;
