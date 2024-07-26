d347ec2804ea31e29555523c069d6a80ae6aa0a3
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-26 16:58:05 +0330

    add user schema

---
+++ src/modules/user/user.swagger.js
@@ -0,0 +1,33 @@
+/**
+ * @swagger
+ * tags:
+ *  name: User
+ *  description: User Module and Routes
+ */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          SendOTP:
+ *              type: object
+ *              required:
+ *                  -   mobile
+ *              properties:
+ *                  mobile:
+ *                      type: string
+
+ */
+
+/**
+ * @swagger
+ *
+ * /user/whoami:
+ *  get:
+ *      summary: Get User Profile
+ *      tags:
+ *          -   User
+ *      responses:
+ *          200:
+ *              description: success
+ */
