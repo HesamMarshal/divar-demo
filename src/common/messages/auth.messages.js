69c0172a4b784ff072f9164dc760584d6845b2b1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-26 13:50:13 +0330

    create message constant

---
+++ src/common/messages/auth.messages.js
@@ -0,0 +1,9 @@
+const AuthorizationMessage = Object.freeze({
+  Login: "Please Login",
+  LoginAgain: "Please Login Again",
+  Unauthorized: "Unauthorized! Please Login",
+  NotFound: "User Not Found",
+  InvalidToken: "Token is invalid",
+});
+
+module.exports = AuthorizationMessage;
