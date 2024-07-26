3b48ff61244033f10c776a58d17813f9bcc14cc2
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-26 20:33:00 +0330

    add schema for get and create new category

---
+++ src/modules/category/category.swagger.js
@@ -0,0 +1,60 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Category
+ *  description: Category Module and Routes
+ */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          CreateCategory:
+ *              type: object
+ *              requiered:
+ *                  - name
+ *                  - icon
+ *              properties:
+ *                  name:
+ *                      type: string
+ *                  slug:
+ *                      type: string
+ *                  icon:
+ *                      type: string
+ *                  parent:
+ *                      type: string
+ */
+
+/**
+ * @swagger
+ * /category/:
+ *  post:
+ *      summary: Create new category
+ *      tags:
+ *          -   Category
+ *      requestBody:
+ *          content:
+ *              application/x-form-urlencoded:
+ *                  schema:
+ *                      $ref: '#/components/schemas/CreateCategory'
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/CreateCategory'
+ */
+
+/**
+ * @swagger
+ * /category/:
+ *  get:
+ *      summary: Get All categories
+ *      tags:
+ *          -   Category
+ *      requestBody:
+ *          content:
+ *              application/x-form-urlencoded:
+ *                  schema:
+ *                      $ref: '#/components/schemas/CreateCategory'
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/CreateCategory'
+ */
