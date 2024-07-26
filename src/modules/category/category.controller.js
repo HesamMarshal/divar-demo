3e7506c34c9b81e94658d4a52669ca45ebf9289b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-26 21:05:25 +0330

    add create method

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,11 +1,23 @@
 const autoBind = require("auto-bind");
 const categoryService = require("./category.service");
+const { CategoryMessage } = require("../../common/messages/messages");
 
 class CategoryController {
   #service;
   constructor() {
     autoBind(this);
     this.#service = categoryService;
+  }
+  async create(req, res, next) {
+    try {
+      const { name, icon, slug, parent } = req.body;
+      // add to database status 201
+      return res.status(201).json({
+        messages: CategoryMessage.CreatedSuccessfully,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 module.exports = new CategoryController();
