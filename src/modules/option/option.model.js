1635a4efe575b139ee648832652454549202a19e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-29 16:40:23 +0330

    create Option Schema and model

---
+++ src/modules/option/option.model.js
@@ -0,0 +1,18 @@
+const { Schema, Types, model } = require("mongoose");
+
+const OptionSchema = new Schema({
+  title: { type: String, required: true },
+  key: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["number", "string", "aaray", "boolean"],
+    required: true,
+  },
+  enum: { type: Array, default: [] },
+  guide: { type: String, required: false },
+  category: { type: Types.ObjectId, ref: "category", required: true },
+});
+
+const OptionModel = model("option", OptionSchema);
+
+module.exports = OptionModel;
