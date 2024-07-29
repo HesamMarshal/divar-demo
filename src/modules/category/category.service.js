9365fbf2c0ff1a561e4f683315bc64e4c7d4dc37
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-07-29 10:35:48 +0330

    add create logic

--- src/modules/category/category.service.js
+++ src/modules/category/category.service.js
@@ -1,5 +1,10 @@
 const autoBind = require("auto-bind");
 const CategoryModel = require("./category.model");
+const { isValidObjectId, Types } = require("mongoose");
+const createHttpError = require("http-errors");
+const { CategoryMessage } = require("../../common/messages/messages");
+const { default: slugify } = require("slugify");
+const e = require("express");
 
 class CategoryService {
   #model;
@@ -7,7 +12,56 @@ class CategoryService {
     autoBind(this);
     this.#model = CategoryModel;
   }
-  async create(categoryDto) {}
+  async create(categoryDto) {
+    // console.log("Start:", categoryDto);
+
+    if (categoryDto?.parent && isValidObjectId(categoryDto.parent)) {
+      const existCategory = await this.checkExistById(categoryDto.parent);
+      categoryDto.parent = existCategory._id;
+      categoryDto.parents = [
+        ...new Set(
+          [existCategory._id.toString()]
+            .concat(existCategory.parents.map((id) => id.toString()))
+            .map((id) => Types.ObjectId(id))
+        ),
+      ];
+    }
+    if (categoryDto?.slug) {
+      categoryDto.slug = slugify(categoryDto.slug);
+      await this.alreadyExistBySlug(categoryDto.slug);
+    } else {
+      categoryDto.slug = slugify(categoryDto.name);
+    }
+
+    // console.log("End:", categoryDto);
+    try {
+      const category = await this.#model.create(categoryDto);
+    } catch (error) {
+      console.log("create:", error);
+    }
+
+    // console.log("End22:", categoryDto);
+    return categoryDto;
+  }
   async find() {}
+
+  async checkExistById(id) {
+    const category = await this.#model.findById(id);
+    if (!category) throw new createHttpError.NotFound(CategoryMessage.NotFound);
+    return category;
+  }
+
+  async checkExistBySlug(slug) {
+    const category = await this.#model.findOne({ slug });
+    if (!category) throw new createHttpError.NotFound(CategoryMessage.NotFound);
+    return category;
+  }
+  async alreadyExistBySlug(slug) {
+    const category = await this.#model.findOne({ slug });
+    if (category)
+      throw new createHttpError.Conflict(CategoryMessage.AlreadyExist);
+
+    return null;
+  }
 }
 module.exports = new CategoryService();
