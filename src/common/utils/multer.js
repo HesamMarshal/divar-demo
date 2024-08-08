2ae967a3f9ca42dd5d6cc9bb7ff7452fa207bf02
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-08 19:29:22 +0330

    configure multer properly to svae files in specific folder and get only pictures

--- src/common/utils/multer.js
+++ src/common/utils/multer.js
@@ -1,4 +1,35 @@
+const fs = require("fs");
+const createHttpError = require("http-errors");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    fs.mkdirSync(path.join(process.cwd(), "public", "uploads"), {
+      recursive: true,
+    });
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const whiteListFormat = [
+      "image/png",
+      "image/jpg",
+      "image/jpeg",
+      "image/webp",
+    ];
+    if (whiteListFormat.includes(file.mimetype)) {
+      const ext = path.extname(file.originalname);
+      const filename = new Date().getTime().toString() + ext;
+      cb(null, filename);
+    } else {
+      cb(new createHttpError.BadRequest("format of pictures are not correct!"));
+    }
+  },
+});
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 3000000,
+  },
+});
 
 module.exports = { upload };
