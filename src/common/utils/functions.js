7d34b26b7064773179a14f95b666143fb64559af
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-08 18:32:31 +0330

    add helper function to remove some properties form req.body

--- src/common/utils/functions.js
+++ src/common/utils/functions.js
@@ -1,7 +1,14 @@
 const isTrue = (value) => ["true", "True", 1, true].includes(value);
 const isFalse = (value) => ["false", "False", 0, false].includes(value);
 
+const removePropertyInObject = (target = {}, propeties = []) => {
+  for (const item of propeties) {
+    delete target[item];
+  }
+  return target;
+};
 module.exports = {
   isTrue,
   isFalse,
+  removePropertyInObject,
 };
