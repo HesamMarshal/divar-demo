54cd1bc3fc88cb573fddd28321166b45c3dc3a98
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-08 18:31:44 +0330

    add helper fuction to get address details

---
+++ src/common/utils/http.js
@@ -0,0 +1,21 @@
+const { default: axios } = require("axios");
+require("dotenv").require();
+const getAddressDetail = async (lat, lng) => {
+  const result = await axios
+    .get(`${process.env.MAP_IR_URL}?lat=${lat}&lng=${lng}`, {
+      headers: {
+        "x-api-key": process.env.MAP_IR_API_KEY,
+      },
+    })
+    .then((res) => res.data);
+
+  return {
+    province: "فارس", //result.province,
+    city: "شیراز", //result.city,
+    district: "قدوسی", //result.region,
+    address: "ارس، شیراز، محله قدوسی غربی، بلوار پاسداران، بلوار", // result.address,
+    coordinate: [lat, lng],
+  };
+};
+
+module.exports = { getAddressDetail };
